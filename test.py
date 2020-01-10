f5c0c78a387eceec0588adccc666991164127127
rannrann <239124260@qq.com>
2020-01-10 11:05:27 +0800

    use bs4 to grap page

---
+++ test.py
@@ -0,0 +1,14 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url="http://dict.cn/rule"
+
+#opening up connection, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+page_soup = soup(page_html,"html.parser")
+phonetic_symbols=page_soup.find_all("bdo")
+print(phonetic_symbols[0],phonetic_symbols[1])
